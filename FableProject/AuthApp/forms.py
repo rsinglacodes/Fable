b749ea07ff489baa425e40e6e5d052d7783126c8
Rachit Goyal <goyalrachit1410@gmail.com>
2025-05-07 11:03:40 +0530

    Delete FableProject directory

--- FableProject/AuthApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import AppUser
-
-class EditProfileForm(forms.ModelForm):
-    class Meta:
-        model = AppUser
-        fields = ['name', 'phone', 'age', 'gender', 'role']  # Add 'role' if you want it editable
